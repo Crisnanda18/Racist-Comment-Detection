799af25af3c223e6772d8e132e536bebfc41afd6
Crisnanda Agung Salvatoni <crisnandasalvatoni@gmail.com>
2024-11-14 15:57:22 +0700

    init commit flask

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
