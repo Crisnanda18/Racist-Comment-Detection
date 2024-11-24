11f37308731d011cbe69a32f18da642544fe33ad
Crisnanda Agung Salvatoni <crisnandasalvatoni@gmail.com>
2024-11-24 13:19:24 +0700

    feat crud posts

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_mysqldb import MySQL
 from datetime import datetime
 import yaml
+import os
+import urllib.request
+
 
 app = Flask(__name__)
 app.secret_key = 'secret'
+upload_folder = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = upload_folder
 
 
 def get_greeting():
@@ -56,6 +61,7 @@ def login():
         if resultValue > 0:
             user = cur.fetchone()
             if password == user[3]:  # Accessing the password field correctly
+                session['id'] = user[0]
                 session['name'] = user[1]
                 session['email'] = user[2]
                 return redirect(url_for('main'))
@@ -68,14 +74,98 @@ def login():
 @app.route('/main', methods=['GET'], endpoint='main')
 def feeds():
     if 'name' in session:
+        sql = "SELECT * FROM posts JOIN users ON posts.user_id = users.id"
+        cur = mysql.connection.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        posts =[]
+        for row in rows:
+            post = {
+                'id': row[0],
+                'caption': row[1],
+                'image': row[2],
+                'user_id': row[3],
+                'created_at': row[4],
+                'name': row[6],
+            }
+            posts.append(post)
         greeting = get_greeting()
-        return render_template('socmed.html', greeting=greeting, name=session['name'],email=session['email'])
+        return render_template('socmed.html', greeting=greeting, name=session['name'], email=session['email'], posts=posts)
     else:
         return redirect(url_for('auth'))
+    
+@app.route('/main/upload-page', methods=['GET'], endpoint='upload-page')
+def upload_page():
+    if 'name' in session:
+        greeting = get_greeting()
+        return render_template('upload.html', greeting=greeting, name=session['name'],email=session['email'])
+    else:
+        return redirect(url_for('auth'))
+
+@app.route('/main/upload', methods=['POST'], endpoint='upload')
+def upload():
+    if 'name' in session:
+        if request.method == 'POST':
+            userDetails = request.form
+            caption = userDetails['caption']
+            image = request.files['image']
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO posts(caption, image, user_id) VALUES(%s, %s, %s)", (caption, image.filename, session['id']))
+            mysql.connection.commit()
+            cur.close()
+            flash('Upload successful', 'success')
+            return redirect(url_for('main'))
+    else:
+        return redirect(url_for('auth'))
+
+@app.route('/main/post/<int:post_id>', methods=['GET'], endpoint='post-detail')
+def post(post_id):
+    if 'name' in session:
+        greeting = get_greeting()
+        post_id = post_id
+        cur = mysql.connection.cursor()
+        sql = "SELECT * FROM posts JOIN users ON posts.user_id = users.id WHERE posts.id = %s"
+        cur.execute(sql, [post_id])
+        postDetail = cur.fetchone()
+        sql = "SELECT * FROM comments JOIN users ON comments.user_id = users.id WHERE post_id = %s"
+        cur.execute(sql, [post_id])
+        comments = cur.fetchall()
+        if postDetail:
+            post_data = {
+                'id': postDetail[0],
+                'caption': postDetail[1],
+                'image': postDetail[2],
+                'user_id': postDetail[3],
+                'created_at': postDetail[4],
+                'name': postDetail[6],
+            }
+            
+        if comments:
+            comments_data = []
+            for comment in comments:
+                comment_data = {
+                    'id': comment[0],
+                    'comment': comment[1],
+                    'post_id': comment[2],
+                    'user_id': comment[3],
+                    'created_at': comment[4],
+                    'name': comment[6],
+                }
+                comments_data.append(comment_data)
+        else:
+            return render_template('comments.html', greeting=greeting, name=session['name'], email=session['email'], post=post_data)
+            
+        return render_template('comments.html', greeting=greeting, name=session['name'], email=session['email'], post=post_data, comments=comments_data)
+    else:
+        return redirect(url_for('auth'))
+    
+
 
 @app.route('/migrate')
 def migrate():
     cur = mysql.connection.cursor()
+    sql_db = "CREATE DATABASE IF NOT EXISTS sara"
     sql_user = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255))"
     sql_posts = """
     CREATE TABLE IF NOT EXISTS posts (
@@ -98,6 +188,7 @@ def migrate():
         FOREIGN KEY(user_id) REFERENCES users(id)
     )
     """
+    cur.execute(sql_db)
     cur.execute(sql_user)
     cur.execute(sql_posts)
     cur.execute(sql_comments)
